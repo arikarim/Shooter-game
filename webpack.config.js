f0dd90b94aa5a99969caf55a89a8877527b9295a
arikarim <arikarim146@gmail.com>
2021-05-05 16:59:07 +0300

    Add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -11,6 +14,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
