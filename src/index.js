77f46362b8796e48df2de1b04f164075696ac710
arikarim <arikarim146@gmail.com>
2021-05-05 17:05:40 +0300

    Add phaser config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
+import Phaser from './phaser';
 import './style.css';
-alert('ll')
+
+
+
+const config = {
+  type: Phaser.WEBGL,
+  width: 600,
+  height: 550,
+  backgroundColor: 'black',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+    },
+  },
+  scene: [
+    StartScreen,
+    MainScene,
+    GameOver,
+    Score,
+  ],
+  pixelArt: true,
+  roundPixels: true,
+};
+
+// eslint-disable-next-line no-unused-vars
+const game = new Phaser.Game(config);
